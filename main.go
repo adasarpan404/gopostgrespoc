624edef51b1d9c3010397c4ed5611d28c8ca2afd
ArpanDas <dasarpan471@gmail.com>
2024-12-24 22:00:06 +0530

    user structure is defined

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Name     string `gorm:"size:100;not null"`
+	Email    string `gorm:"size:100;unique;not null"`
+	Password string `gorm:"size:255;not null"`
+}
 func main(){
 	dsn := "postgres://<user>:<password>@<host>/<database>?sslmode=require"
 }
