0b59422552dcfa98dee938623e27d0adda112ba4
ArpanDas <dasarpan471@gmail.com>
2025-01-14 07:36:19 +0530

    models is defined

--- model/user.go
+++ model/user.go
@@ -1,1 +1,15 @@
 package model
+
+type User struct {
+	ID       uint   `gorm:"primarykey" json:"id"`
+	Name     string `gorm:"size:100;not null" json:"name"`
+	Email    string `gorm:"size:100;not null" json:"email"`
+	Password string `gorm:"size:100;not null" json:"password"`
+}
+
+type Profile struct {
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	UserID   uint   `gorm:"unique;not null" json:"user_id"`
+	Bio      string `gorm:"size:255" json:"bio"`
+	Location string `gorm:"size:100" json:"location"`
+}
