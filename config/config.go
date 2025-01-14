44b1d57e7cfe6bd5f6ad922a4aeafc5aa808db1a
ArpanDas <dasarpan471@gmail.com>
2025-01-14 06:36:10 +0530

    working on config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,24 @@
 package config
 
 import (
+	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
 	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error While Loading .env file: %v", err)
+	}
+
+	dsn := os.Getenv("NEON_DATABASE_URL")
+
+	if dsn == "" {
+		log.Fatalf("There is no Database URL set in the enviroment")
+	}
+
 }
